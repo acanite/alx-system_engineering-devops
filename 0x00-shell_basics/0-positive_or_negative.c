dafc88d7acff89b5f9030d6e2244274264a92342
acanite <acanite01@gmail.com>
2022-06-06 05:24:27 -0700

    0-positive_or_negative.c

--- 0x00-shell_basics/0-positive_or_negative.c
+++ 0x00-shell_basics/0-positive_or_negative.c
@@ -5,10 +5,10 @@
 /**
  * main - Prints if number is positive, zero or negative
  *
- * Return: Always (Success)
+ * Return:  Always (Success)
  * /
- int main(void)
- { 
+  int main(void)
+  { 
         int n;
 
 	srand(time(0));
@@ -17,14 +17,13 @@
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
-	}
-	else if (n == 0)
+	} else if (n == 0)
 	{
-	        printf("%d is zero\n", n);
-	}
-       	 else
+		printf("%d is zero\n", n);
+	} else
 	{
 		printf("%d is negative\n", n);
 	}
+
 	return (0);
- }
+  }
---
+++ 0x00-shell_basics/README.md
@@ -0,0 +1,4 @@
+ls
+ls -1
+cd 0-positive_or_negative.c
+vi 0-positive_or_negative.c
